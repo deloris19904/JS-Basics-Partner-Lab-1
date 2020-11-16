111293a2c44f846c590728dc77b7203ac31122f1
Victoria Konkiel <victoria.konkiel@yahoo.com>
2020-11-16 14:32:51 -0500

    started part 3

--- mockup.js
+++ mockup.js
@@ -11,4 +11,14 @@ alert("Fizz")
 alert("Buzz")
 }
 
+let numberRange = 6
+let userGuess = prompt("Pick a number between 1 and 10")
+if (userGuess === numberRange) {
+    alert("That is correct!")
+} else {
+    alert("Sorry, try another number.")
+}
 
+
+Generate a number between 1-10. 
+Ask the user to guess the number and display if the user guessed correctly or not.
