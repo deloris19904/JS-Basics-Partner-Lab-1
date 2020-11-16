650b98473041125a1a81b22b96aa66e708122da4
deloris19904 <73267872+deloris19904@users.noreply.github.com>
2020-11-16 14:16:28 -0500

    completed part 1 and 2 of mockup

---
+++ mockup.js
@@ -0,0 +1,14 @@
+let number1 = prompt (" pick a number")
+let number2 = prompt (" pick a second number")
+let result = number1/number2
+alert (result) 
+let number = prompt ("enter a number")
+if ((number%3)===0 && (number%5)===0)
+{ alert("FizzBuzz")}
+else if((number%3)===0){
+alert("Fizz")
+}else if ((number%5)===0){
+alert("Buzz")
+}
+
+
